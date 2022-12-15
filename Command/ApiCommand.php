c0f3908fd5e5f08c718b51e103f7a45b3f83e279
Louis Ouellet <louis_ouellet@hotmail.com>
2022-12-15 17:07:16 -0500

    2022-12-15 17:07 - v1.0.31

---
+++ Command/ApiCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+//Import BaseCommand class into the global namespace
+use LaswitchTech\phpCLI\BaseCommand;
+
+class ApiCommand extends BaseCommand {
+
+  public function getAction($argv){}
+
+  public function postAction($argv){}
+}
