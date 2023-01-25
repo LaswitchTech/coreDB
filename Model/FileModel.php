8dbef92886fcff88fd922a82076659e8070d07bc
Louis Ouellet <louis_ouellet@hotmail.com>
2023-01-25 11:15:31 -0500

    2023-01-25 11:15 - v1.0.61

--- Model/FileModel.php
+++ Model/FileModel.php
@@ -73,15 +73,7 @@ class FileModel extends BaseModel {
     if(substr($dirPath, strlen($dirPath) - 1, 1) != '/'){
       $dirPath .= '/';
     }
-    $files = glob($dirPath . '*', GLOB_MARK);
-    foreach($files as $file){
-      if(is_dir($file)){
-        $this->deleteDir($file);
-      } else {
-        unlink($file);
-      }
-    }
-    $files = glob($dirPath . '.*', GLOB_MARK);
+    $files = glob($dirPath . '{,.}*[!.]*', GLOB_MARK | GLOB_BRACE);
     foreach($files as $file){
       if(is_dir($file)){
         $this->deleteDir($file);
